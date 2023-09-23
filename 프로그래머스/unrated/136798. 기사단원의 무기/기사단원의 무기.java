ec90489cda30a5fe6fead183206f81caf0868165
Murphy <111736036+taekk1a2a3a@users.noreply.github.com>
2023-09-24 03:14:00 +0900

    [unrated] Title: 기사단원의 무기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/unrated/136798. 기사단원의 무기/기사단원의 무기.java
+++ 프로그래머스/unrated/136798. 기사단원의 무기/기사단원의 무기.java
@@ -1,26 +1,28 @@
 class Solution {
     public int solution(int number, int limit, int power) {
         int answer = 0;
-        int count = 0;
-        // i 마다 약수의 갯수 구해야함.
+        
         for (int i = 1; i <= number; i++) {
-            for (int j = 1; j * j <= i ; j++) {
-                if (i % j == 0 ){
+            int count = 0; // 각 숫자마다 약수의 갯수 초기화
+            
+            // i의 약수 구하기
+            for (int j = 1; j * j <= i; j++) {
+                if (i % j == 0) {
                     count++;
-                    if (j * j < i){
+                    if (j * j < i) {
                         count++;
                     }
                 }
-
             }
-            if (count > limit){
+            
+            // 약수의 갯수에 따라 결과값 계산
+            if (count > limit) {
                 answer += power;
-                count = 0;
             } else {
                 answer += count;
-                count = 0;
             }
         }
+
         return answer;
     }
 
