a55ac1d44f1ebbb53ff0f6c0f4f6e7423366f7a8
Murphy <111736036+taekk1a2a3a@users.noreply.github.com>
2024-01-28 19:47:17 +0900

    [level 0] Title: 문자열 잘라서 정렬하기, Time: 17.95 ms, Memory: 87.1 MB -BaekjoonHub

--- 프로그래머스/0/181866. 문자열 잘라서 정렬하기/문자열 잘라서 정렬하기.java
+++ 프로그래머스/0/181866. 문자열 잘라서 정렬하기/문자열 잘라서 정렬하기.java
@@ -1,34 +1,20 @@
-import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Solution {
     public String[] solution(String myString) {
-        ArrayList<String> list = new ArrayList<>();
-        StringBuilder sb = new StringBuilder();
-        myString += "x";
-        for (char c : myString.replaceAll(" ", "").toCharArray()) {
-            if (c != 'x'){
-                sb.append(c);
-            } else {
-                if (sb.length() > 0){
-                    list.add(sb.toString());
-                }
-                sb.setLength(0);
-            }
-        }
+        String[] answer  = myString.replaceAll(" ", "").split("x");
 
-        String[] answer = new String[list.size()];
-        for (int i = 0; i < list.size(); i++) {
-            answer[i] = list.get(i);
-        }
-
+        answer = Arrays.stream(answer)
+                .filter(s -> !s.isEmpty())
+                .toArray(String[]::new);
+        
         Arrays.sort(answer);
         return answer;
     }
 
     public static void main(String[] args) {
         Solution sol = new Solution();
-        String myString = " dxccxbbbxaaaa";
+        String myString = "dxccxbbbxaaaa";
         String[] answer = sol.solution(myString);
         System.out.println("Arrays.toString(answer) = " + Arrays.toString(answer));
     }
