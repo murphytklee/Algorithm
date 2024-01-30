5c165a0cd074042b9dfc67148c0a01d4e9878781
Murphy <111736036+taekk1a2a3a@users.noreply.github.com>
2024-01-30 20:08:44 +0900

    [level 0] Title: 정수 찾기, Time: 0.02 ms, Memory: 76.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181840. 정수 찾기/정수 찾기.java
@@ -0,0 +1,19 @@
+public class Solution {
+    public int solution(int[] num_list, int n) {
+        int answer = 0;
+        for (int num : num_list) {
+            if (num == n){
+                answer = 1;
+            }
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        int[] num_list = {1, 2, 3, 4, 5};
+        int n = 3;
+        int answer = sol.solution(num_list, n);
+        System.out.println("answer = " + answer);
+    }
+}
