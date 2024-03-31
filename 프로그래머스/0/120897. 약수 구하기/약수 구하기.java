42eebb43e93100cb7573b6805d6de7ad0bc20bca
Murphy <111736036+taekk1a2a3a@users.noreply.github.com>
2024-03-31 18:02:54 +0900

    [level 0] Title: 약수 구하기, Time: 0.19 ms, Memory: 77.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120897. 약수 구하기/약수 구하기.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
+class Solution {
+    public int[] solution(int n) {
+        ArrayList<Integer> divisors = new ArrayList<>();
+        
+        for (int i = 1; i <= n; i++) {
+            if (n % i == 0) {
+                divisors.add(i);
+            }
+        }
+        
+        Collections.sort(divisors);
+        
+        int[] answer = new int[divisors.size()];
+        for (int i = 0; i < divisors.size(); i++) {
+            answer[i] = divisors.get(i);
+        }
+        
+        return answer;
+    }
+}
