99bb721dc43b622f5dea6aa2558d94e2bdc068e2
Murphy <111736036+taekk1a2a3a@users.noreply.github.com>
2024-04-14 20:40:27 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 688 ms, Memory: 142208 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
@@ -9,38 +9,41 @@ public class Main {
     static List<List<Integer>> graph;
 
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken()); // 정점 개수
-        M = Integer.parseInt(st.nextToken()); // 간선 개수
-        graph = new ArrayList<>();
-        visited = new boolean[N + 1];
-        connectedComponent = 0;
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+             BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out))) {
 
-        for (int i = 0; i <= N; i++) {
-            graph.add(new ArrayList<>());
-        }
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            N = Integer.parseInt(st.nextToken()); // 정점 개수
+            M = Integer.parseInt(st.nextToken()); // 간선 개수
 
-        for (int i = 0; i < M; i++) {
-            st = new StringTokenizer(br.readLine());
-            int u = Integer.parseInt(st.nextToken());
-            int v = Integer.parseInt(st.nextToken());
-            graph.get(u).add(v);
-            graph.get(v).add(u);
-        }
-        for (int i = 1; i <= N; i++) {
-            if (!visited[i]) {
-                dfs(i);
-                connectedComponent++;
+            graph = new ArrayList<>(N + 1);
+            visited = new boolean[N + 1];
+            connectedComponent = 0;
+
+            for (int i = 0; i <= N; i++) {
+                graph.add(new ArrayList<>());
             }
-        }
 
-        bw.write(Integer.toString(connectedComponent));
-        bw.flush();
-        bw.close();
+            for (int i = 0; i < M; i++) {
+                st = new StringTokenizer(br.readLine());
+                int u = Integer.parseInt(st.nextToken());
+                int v = Integer.parseInt(st.nextToken());
+                graph.get(u).add(v);
+                graph.get(v).add(u);
+            }
+
+            for (int i = 1; i <= N; i++) {
+                if (!visited[i]) {
+                    dfs(i);
+                    connectedComponent++;
+                }
+            }
+
+            bw.write(Integer.toString(connectedComponent));
+        }
     }
-    private static void dfs(int vertex){
+
+    private static void dfs(int vertex) {
         visited[vertex] = true;
         for (int neighbor : graph.get(vertex)) {
             if (!visited[neighbor]) {
