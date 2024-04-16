75ee0d0aed8796cf71c1a7972e5e2527c3a172f6
Murphy <111736036+taekk1a2a3a@users.noreply.github.com>
2024-04-16 20:09:11 +0900

    [level 0] Title: 종이 자르기, Time: 0.01 ms, Memory: 73.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120922. 종이 자르기/종이 자르기.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(int M, int N) {
+        int answer = 0;
+        
+        if (M == 1 && N == 1){
+            return answer;
+        }
+        
+        answer = (M - 1) + (N - 1) * M;
+        
+        return answer;
+    }
+}
