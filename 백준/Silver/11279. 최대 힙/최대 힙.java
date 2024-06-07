8f9e10dbae086aeb59d834de4934a4535bf2303e
Murphy <111736036+murphytklee@users.noreply.github.com>
2024-06-07 22:45:35 +0900

    [Silver II] Title: 최대 힙, Time: 396 ms, Memory: 26932 KB -BaekjoonHub

---
+++ 백준/Silver/11279. 최대 힙/최대 힙.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
+import java.util.PriorityQueue;
+import java.util.Comparator;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int N = Integer.parseInt(br.readLine());
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Comparator.reverseOrder());
+
+        for (int i = 0; i < N; i++) {
+            int x = Integer.parseInt(br.readLine());
+
+            if (x == 0) {
+                if (maxHeap.isEmpty()) {
+                    bw.write("0\n");
+                } else {
+                    bw.write(maxHeap.poll() + "\n");
+                }
+            } else {
+                maxHeap.add(x);
+            }
+        }
+
+        bw.flush();
+        br.close();
+        bw.close();
+    }
+}
