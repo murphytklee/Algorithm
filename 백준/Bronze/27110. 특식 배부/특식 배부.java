6a375a944454d998e28bb32f46a4ce57692b5586
Murphy <111736036+murphytklee@users.noreply.github.com>
2024-06-24 20:03:56 +0900

    [Bronze IV] Title: 특식 배부, Time: 124 ms, Memory: 14144 KB -BaekjoonHub

---
+++ 백준/Bronze/27110. 특식 배부/특식 배부.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int N = Integer.parseInt(br.readLine().trim());
+        
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+        int C = Integer.parseInt(st.nextToken());
+        
+        int maxSatisfied = Math.min(N, A) + Math.min(N, B) + Math.min(N, C);
+        
+        System.out.println(maxSatisfied);
+    }
+}
