6d4154eea03dbf93dcb6638c94100a890a1c634c
Murphy <111736036+murphytklee@users.noreply.github.com>
2024-07-18 09:32:20 +0900

    [level 0] Title: 배열 만들기 3, Time: 2.78 ms, Memory: 43.7 MB -BaekjoonHub

---
+++ JavaScript/프로그래머스/0/181895. 배열 만들기 3/배열 만들기 3.js
@@ -0,0 +1,8 @@
+function solution (arr, intervals) {
+    const answer = [];
+    
+    answer.push(...arr.slice(intervals[0][0], intervals[0][1] + 1));
+    answer.push(...arr.slice(intervals[1][0], intervals[1][1] + 1));
+
+    return answer;
+};
