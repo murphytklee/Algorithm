7f5e6ecbbfcae94790756134c65cd7b58e9adc1e
Murphy <111736036+murphytklee@users.noreply.github.com>
2024-07-18 10:39:16 +0900

    [level 0] Title: 배열 만들기 5, Time: 2.37 ms, Memory: 41 MB -BaekjoonHub

--- JavaScript/프로그래머스/0/181912. 배열 만들기 5/배열 만들기 5.js
+++ JavaScript/프로그래머스/0/181912. 배열 만들기 5/배열 만들기 5.js
@@ -2,9 +2,9 @@ function solution (intStrs, k, s, l) {
     const answer = [];
 
     intStrs.forEach(num => {
-        const sliceNum = num.slice(s, s + l);
+        const sliceNum = parseInt(num.slice(s, s + l));
         if(sliceNum > k) {
-            answer.push(parseInt(sliceNum));
+            answer.push(sliceNum);
         }
     });
 
