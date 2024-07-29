efa736415718194d480094771806717637c96e6b
Murphy <111736036+murphytklee@users.noreply.github.com>
2024-07-30 08:40:46 +0900

    [level 1] Title: 음양 더하기, Time: 0.11 ms, Memory: 33.4 MB -BaekjoonHub

--- JavaScript/프로그래머스/1/76501. 음양 더하기/음양 더하기.js
+++ JavaScript/프로그래머스/1/76501. 음양 더하기/음양 더하기.js
@@ -1,7 +1,11 @@
 function solution(absolutes, signs) {
     let answer = 0;
-    for (let i = 0; i < absolutes.length; ++i) {
-        signs[i] ? answer += absolutes[i] : answer -= absolutes[i]
-    }
+    absolutes.forEach((v, i) => {
+        if (signs[i]) {
+            answer += v;
+        } else {
+            answer -= v;
+        }
+    })
     return answer;
 }
