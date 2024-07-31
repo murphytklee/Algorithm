401c4d63994eb1d0b40849f585a24e4af1d5350d
Murphy <111736036+murphytklee@users.noreply.github.com>
2024-08-01 08:28:48 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 0.17 ms, Memory: 33.4 MB -BaekjoonHub

--- JavaScript/프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.js
+++ JavaScript/프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.js
@@ -1,19 +1,11 @@
 function solution(left, right) {
-    let answer = 0;
-
+    var answer = 0;
     for (let i = left; i <= right; i++) {
-        let count = 0;
-        for (let j = 1; j <= i; j++) {
-            if (i % j === 0) {
-                count++;
-            }
-        }
-        if (count % 2 === 0) {
-            answer += i;
-        } else {
+        if (Number.isInteger(Math.sqrt(i))) {
             answer -= i;
+        } else {
+            answer += i;
         }
     }
-    
     return answer;
 }
