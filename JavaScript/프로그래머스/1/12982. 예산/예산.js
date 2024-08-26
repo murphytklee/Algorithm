64cce7c1e8ec169831bb04edfac932bd95572183
Murphy <111736036+murphytklee@users.noreply.github.com>
2024-08-27 08:31:46 +0900

    [level 1] Title: 예산, Time: 0.17 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ JavaScript/프로그래머스/1/12982. 예산/예산.js
@@ -0,0 +1,13 @@
+function solution(d, budget) {
+    let answer = 0;
+    d.sort((a, b) => a - b);
+    for(let i = 0; i < d.length; i++) {
+        budget -= d[i];
+        answer++;
+        if (budget < 0){
+            answer--;
+            break;
+        }
+    }
+    return answer;
+}
