8f73585d41ce00dcd4f16d6398811a76329d1d15
Murphy <111736036+murphytklee@users.noreply.github.com>
2024-09-10 15:48:16 +0900

    [level 1] Title: K번째수, Time: 0.08 ms, Memory: 33.5 MB -BaekjoonHub

--- JavaScript/프로그래머스/1/42748. K번째수/K번째수.js
+++ JavaScript/프로그래머스/1/42748. K번째수/K번째수.js
@@ -1,11 +1,7 @@
 function solution(array, commands) {
-    let answer = [];
-    for (let t = 0; t < commands.length; t++) {
-        let i = commands[t][0] - 1;
-        let j = commands[t][1];
-        let k = commands[t][2] - 1;
-        
-        answer.push(array.slice(i, j).sort((a, b) => a - b)[k]);
-    }
-    return answer;
+    return commands.map( command => {
+        const [start, end, position] = command;
+        return array.slice(start - 1, end)
+                    .sort((a, b) => a - b)[position - 1];
+    })
 }
