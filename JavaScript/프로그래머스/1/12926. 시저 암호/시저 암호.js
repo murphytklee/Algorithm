a16bf03fab5f4a1fd29d74007224749df8cd489f
Murphy <111736036+murphytklee@users.noreply.github.com>
2024-09-10 14:20:53 +0900

    [level 1] Title: 시저 암호, Time: 1.96 ms, Memory: 36.9 MB -BaekjoonHub

---
+++ JavaScript/프로그래머스/1/12926. 시저 암호/시저 암호.js
@@ -0,0 +1,15 @@
+function solution(s, n) {
+    return s.split('').map(char => {
+        const code = char.charCodeAt(0);
+        
+        if (code >= 65 && code <= 90) {
+            return String.fromCharCode(((code - 65 + n) % 26) + 65);
+        }
+        
+        else if (code >= 97 && code <= 122) {
+            return String.fromCharCode(((code - 97 + n) % 26) + 97);
+        }
+
+        return char;
+    }).join('');
+}
