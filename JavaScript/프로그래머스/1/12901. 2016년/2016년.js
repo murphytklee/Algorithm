40b5d6cb57eea9bffc003016b4d204fa3e180a0b
Murphy <111736036+murphytklee@users.noreply.github.com>
2024-09-13 09:17:00 +0900

    [level 1] Title: 2016년, Time: 0.15 ms, Memory: 33.8 MB -BaekjoonHub

--- JavaScript/프로그래머스/1/12901. 2016년/2016년.js
+++ JavaScript/프로그래머스/1/12901. 2016년/2016년.js
@@ -1,6 +1,5 @@
 function solution(a, b) {
     const week = ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT'];
-    const day = '2016-' + a + '-' + b;
-    
-    return week[new Date(day).getDay()];
+
+    return week[new Date(`2016-${a}-${b}`).getDay()];
 }
