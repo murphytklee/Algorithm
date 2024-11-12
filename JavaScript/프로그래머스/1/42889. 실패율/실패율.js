6137ef268794e21e8481640db750dea2821f242a
Murphy <111736036+murphytklee@users.noreply.github.com>
2024-11-12 10:43:34 +0900

    [level 1] Title: 실패율, Time: 5913.36 ms, Memory: 67.1 MB -BaekjoonHub

--- JavaScript/프로그래머스/1/42889. 실패율/실패율.js
+++ JavaScript/프로그래머스/1/42889. 실패율/실패율.js
@@ -1,27 +1,11 @@
 function solution(N, stages) {
-	let stageCounts = {};
-	let playerCount = stages.length;
-	let failureRates = [];
-	
-	for (let i = 1; i <= N; i++) {
-	  stageCounts[i] = 0;
-	}
-	
-	stages.forEach(stage => {
-		if (stage in stageCounts) {
-			stageCounts[stage]++;
-		}
-	});
-	
+    let result = [];
+
     for (let i = 1; i <= N; i++) {
-        if (playerCount === 0) {
-            failureRates.push([i, 0]);
-        } else {
-            let failureRate = stageCounts[i] / playerCount;
-            failureRates.push([i, failureRate]);
-            playerCount -= stageCounts[i];
-        }
+        let reach = stages.filter((x) => x >= i).length;
+        let curr = stages.filter((x) => x === i).length;
+        result.push([i, curr/reach]);
     }
-	
-    return failureRates.sort((a, b) => b[1] - a[1] || a[0] - b[0]).map(item => item[0]);
+    ;
+    return result.sort((a,b) => b[1] - a[1]).map((x) => x[0]);
 }
