3fd832070d3c94fa120be8a045d014c3001c26fa
Murphy <111736036+murphytklee@users.noreply.github.com>
2024-11-14 10:23:59 +0900

    [level 1] Title: 대충 만든 자판, Time: 1.79 ms, Memory: 33.7 MB -BaekjoonHub

--- JavaScript/프로그래머스/1/160586. 대충 만든 자판/대충 만든 자판.js
+++ JavaScript/프로그래머스/1/160586. 대충 만든 자판/대충 만든 자판.js
@@ -2,26 +2,13 @@ function solution(keymap, targets) {
     let answer = [];
     let map = {};
 
-    for (let key of keymap) {
-        for (let i = 0; i < key.length; i++) {
-            if (map[key[i]] === undefined || i < map[key[i]]) {
-                map[key[i]] = i;
-            }
-        }
-    }
-    
-    for (target of targets) {
-        let count = 0;
-        for (let i = 0; i < target.length; i++) {
-            if (map[target[i]] !== undefined) {
-                count += map[target[i]] + 1;
-            } else {
-                count = -1;
-                break;
-            }
-        }
-        answer.push(count);
+    for (const key of keymap) {
+        key.split('').map((key, index) => map[key] = (map[key] < index + 1 ? map[key] : index + 1));
     }
 
+    for (const target of targets) {
+        answer.push(target.split('').reduce((cur, target) => cur += map[target], 0) || -1);
+    }
+    
     return answer;
 }
