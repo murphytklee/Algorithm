69916aafaf73ead940489f134f6d26cbef80b4f8
Murphy <111736036+murphytklee@users.noreply.github.com>
2024-11-14 11:21:22 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 60.50 ms, Memory: 66.8 MB -BaekjoonHub

--- JavaScript/프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
+++ JavaScript/프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
@@ -1,12 +1,15 @@
 function solution(participant, completion) {
-    let answer = '';
-    participant.sort();
-    completion.sort();
+    const map = new Map();
+
     for (let i = 0; i < participant.length; i++) {
-        if (participant[i] != completion[i]) {
-            answer = participant[i];
-            break;
-        }
+        let a = participant[i];
+        let b = completion[i];
+
+        map.set(a, (map.get(a) || 0) + 1);
+        map.set(b, (map.get(b) || 0) - 1);
     }
-    return answer;
+
+    for (let [k, v] of map) {
+        if (v > 0) return k;
+    }
 }
