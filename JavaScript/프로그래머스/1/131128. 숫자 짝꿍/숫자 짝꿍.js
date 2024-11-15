53d33f0fd87f7c5165d9d5962702a370a27bdd4e
Murphy <111736036+murphytklee@users.noreply.github.com>
2024-11-15 14:36:37 +0900

    [level 1] Title: 숫자 짝꿍, Time: 1809.13 ms, Memory: 209 MB -BaekjoonHub

--- JavaScript/프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.js
+++ JavaScript/프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.js
@@ -1,19 +1,19 @@
 function solution(X, Y) {
     const count = new Map();
-    let answer = '';
+    const arr = [];
 
     for (let num of X) {
         count.set(num, (count.get(num) || 0) + 1);
     }
-    
+
     for (let num of Y) {
         if (count.has(num) && count.get(num) > 0) {
+            arr.push(num);
             count.set(num, count.get(num) - 1);
-            answer += num;
         }
     }
 
-    answer = [...answer].sort((a, b) => b - a).join('');
+    const answer = arr.sort((a, b) => b - a).join('');
 
     return answer === '' ? "-1" : answer[0] === '0' ? "0" : answer;
 }
