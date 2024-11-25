1576924883052b4f86602fa32a74b177e3a3211f
Murphy <111736036+murphytklee@users.noreply.github.com>
2024-11-25 11:18:11 +0900

    [level 1] Title: 신규 아이디 추천, Time: 0.23 ms, Memory: 33.5 MB -BaekjoonHub

--- JavaScript/프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.js
+++ JavaScript/프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.js
@@ -1,14 +1,11 @@
 function solution(new_id) {
-    let answer = new_id.toLowerCase();
-    answer = answer.replace(/[^0-9a-z-_.]/g, "");
-    answer = answer.replace(/\.{2,}/g, ".");
-    answer = answer.replace(/^\.|\.$/g, "");
-    if (answer === "") answer = "a";
-    if (answer.length >= 16) {
-        answer = answer.substring(0, 15).replace(/\.$/, "");
-    }
-    while (answer.length < 3) {
-        answer += answer.charAt(answer.length - 1);
-    }
-    return answer;
+    const id = new_id
+        .toLowerCase()
+        .replace(/[^\w\d-_.]/g, '')
+        .replace(/\.{2,}/g, '.')
+        .replace(/^\.|\.$/g, '')
+        .padEnd(1, 'a')
+        .slice(0, 15)
+        .replace(/^\.|\.$/g, '')        
+    return id.padEnd(3, id[id.length-1])
 }
