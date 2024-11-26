e5ee3403e70712af56f6ef006e2836308947ba74
Murphy <111736036+murphytklee@users.noreply.github.com>
2024-11-26 17:02:52 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- JavaScript/프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.js
+++ JavaScript/프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.js
@@ -1,4 +1,4 @@
 function solution(s) {
-    arr = s.split(' ').map(n => parseInt(n)).sort((a, b) => a - b);
-    return arr[0] + ' ' + arr[arr.length - 1]; 
+    let arr = s.split(' ');
+    return Math.min(...arr)+' '+Math.max(...arr);
 }
