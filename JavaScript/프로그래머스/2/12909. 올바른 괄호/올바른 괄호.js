53d8c5b7ca07038fc32b8dd4fdbf50c5ea3af6bd
Murphy <111736036+murphytklee@users.noreply.github.com>
2024-11-26 13:56:21 +0900

    [level 2] Title: 올바른 괄호, Time: 33.19 ms, Memory: 38 MB -BaekjoonHub

---
+++ JavaScript/프로그래머스/2/12909. 올바른 괄호/올바른 괄호.js
@@ -0,0 +1,14 @@
+function solution(s){
+    let stack = [];
+
+    for (let c of s) {
+        if (c == '(') {
+            stack.push(c);
+        } else {
+            if (stack.length == 0) return false;
+            stack.pop();
+        }
+    }
+    
+    return stack.length == 0 ? true : false;
+}
