9e91be3d3620a884f2bfdd26617223f9de9ccb73
Murphy <111736036+murphytklee@users.noreply.github.com>
2024-12-11 11:05:12 +0900

    [level 2] Title: 할인 행사, Time: 21.42 ms, Memory: 42.1 MB -BaekjoonHub

--- JavaScript/프로그래머스/2/131127. 할인 행사/할인 행사.js
+++ JavaScript/프로그래머스/2/131127. 할인 행사/할인 행사.js
@@ -1,19 +1,17 @@
 function solution(want, number, discount) {
     var answer = 0;
-    let totalAmt = number.reduce((acc, cur) => {return acc + cur;}, 0);
     
-    for (let i = 0; i <= discount.length - totalAmt; i++) {
-        let test = discount.slice(i, i + totalAmt);
-        let temp = number.slice();
-
-        for (let j = 0; j < test.length; j++) {
-            let index = want.indexOf(test[j]);
-            temp[index] -= 1;
-        }
+    for (let i = 0; i <= discount.length - 10; i++) {
+        const slice = discount.slice(i, i + 10);
+        let flag = true;
         
-        if (temp.every(el => el === 0)) {
-            answer++;
+        for (let j = 0; j < want.length; j++) {
+            if (slice.filter(item => item === want[j]).length !== number[j]) {
+                flag = false;
+                break;
+            }
         }
+        if (flag) answer++;
     }
     return answer;
 }
